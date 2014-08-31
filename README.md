8f1ad3e8600051276f47536136d7784a65135191
marob <marob@smile.fr>
2014-08-31 18:01:30 +0200

    Externalize configuration + bugfixes + filling info in package.json + README update + architecture diagram

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-npmjs-proxy
-===========
+# npmjs-enterprise
+
+npmjs for enterprise
+
+## Features
+
+   * central registry downtime free
+   * reduces bandwidth usage
+   * speeds up packages download
+   * private packages
+
+### Architecture
+
+![Architecture diagram](/resources/architecture.png)
+
+## Prerequisites
+
+   * install npm-registry-couchapp (follow https://github.com/npm/npm-registry-couchapp#installing)
+   * replicate the skimdb registry (follow https://github.com/npm/npm-registry-couchapp#replicating-the-registry)
+
+## Install
+
+First, clone the repo. Then:
+
+```sh
+npm install
+```
+
+## Configure
+
+   * Copy the "config.js.sample" file as "config.js"
+   * Edit the "config.js" configuration file to fit your needs
+
+## Run
+
+```sh
+node app.js
+```
+
+## Use
+
+On the npm user computer, configure npm to use your proxy:
+
+```sh
+npm set registry "[proxyURL]"
+```
--- app.js
+++ app.js
@@ -5,20 +5,14 @@ var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var request = require('request');
+var replacestream = require('replacestream');
 require('string.prototype.endswith');
 
 var log4js = require('log4js');
 log4js.configure('log4js.json', {reloadSecs: 10});
 var logger = log4js.getLogger('app');
 
-var config = {
-  'port': 1337,
-  'localURL': 'http://localhost:5984/registry/_design/app/_rewrite',
-  'centralURL': 'http://registry.npmjs.org',
-  'regex': new RegExp('http://registry.npmjs.org', 'g'),
-  'attachmentURL': 'http://localhost:1337',
-  'attachmentPath': '/tmp/npmAttachments'
-}
+var config = require('./lib/configLoader');
 
 function manageAttachment(req, resp) {
   var filePath = path.join(config.attachmentPath, req.url);
@@ -38,16 +32,29 @@ function manageAttachment(req, resp) {
   });
 }
 
+// Inspired from https://github.com/mojombo/semver/issues/110#issuecomment-19433284, but more permissive (allow leading zeros) and non capturing
+var versionRegexp = '(\\d+\\.\\d+\\.\\d+(?:-(?:0|[1-9]\\d*|\\d*[a-zA-Z-][a-zA-Z0-9-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][a-zA-Z0-9-]*))*)?(?:\\+[0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*)?)'
+var tarballRegexp = new RegExp('^\\/([^/]+)\\/-\\/[^/]+?-' + versionRegexp + '\\.tgz$');
+
 http.createServer(function (req, resp) {
   logger.debug(req.url);
-  if (req.url.endsWith('.tgz')) {
+
+  if (req.url.match(tarballRegexp) != null) {
     manageAttachment(req, resp);
   } else {
-    logger.debug(req.url + ' : forwarding to couchDB and replacing attachment URLs');
-    request.get(config.localURL + req.url, function (error, response, body) {
-      body = body.replace(config.regex, config.attachmentURL);
-      resp.end(body);
-    });
+    logger.debug(req.url + ' : forwarding to couchDB');
+
+    // Read AND Write stream
+    var couchDBStream = request(config.couchURL + req.url);
+    // Proxy the request to couchDB
+    req.pipe(couchDBStream);
+    // We only replace attachment URLs in GET
+    if (req.method === 'GET') {
+      logger.debug('Responding with attachment URL replacement');
+      couchDBStream.pipe(replacestream(config.centralURL, config.proxyURL)).pipe(resp);
+    } else {
+      couchDBStream.pipe(resp);
+    }
   }
 }).listen(config.port, function () {
     logger.info('Server running at http://localhost:%s with config:\n%s', config.port, JSON.stringify(config, null, 4));
