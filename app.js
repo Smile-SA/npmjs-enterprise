c0eac88080a45cfa3b97d873a6ecd70d22c6ab2c
marob <marob@smile.fr>
2016-06-17 19:03:11 +0200

    Refactor: url info extraction in urlUtils.js + add Unit Tests

--- app.js
+++ app.js
@@ -3,14 +3,10 @@
 var http = require('http');
 
 var config = require('./lib/configLoader');
+var urlUtils = require('./lib/urlUtils');
 var api = require('./lib/api');
 
 var logger = config.logger;
-
-// Inspired from https://github.com/mojombo/semver/issues/110#issuecomment-19433284, but more permissive (allow leading zeros) and non capturing
-var versionRegexp = '(\\d+\\.\\d+\\.\\d+(?:-(?:0|[1-9]\\d*|\\d*[a-zA-Z-][a-zA-Z0-9-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][a-zA-Z0-9-]*))*)?(?:\\+[0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*)?)';
-var tarballRegexp = new RegExp('^(\\/@([^/]+))?\\/([^/]+)\\/-\\/[^/]+?-' + versionRegexp + '\\.tgz$');
-var moduleRegexp = new RegExp('^\\/[^/]+$');
 
 process.on('uncaughtException', function(err) {
   logger.error('Uncaught exception: %s', JSON.stringify(err, null, 4));
@@ -21,16 +17,14 @@ api.initChangesSync();
 http.createServer(function(req, resp) {
   try {
     var isGet = req.method === 'GET';
-    var tarballMatch = req.url.match(tarballRegexp);
-    var isTarball = tarballMatch != null;
-    var isModule = req.url.match(moduleRegexp) != null;
 
-    if (isGet && isTarball) {
-      var module = tarballMatch[3];
-      var version = tarballMatch[4];
-      api.manageAttachment(req, resp, module, version);
-    } else if (isGet && isModule) {
-      api.manageModule(req, resp);
+    var module = urlUtils.module(req.url);
+    var tarball = urlUtils.tarball(req.url);
+
+    if (isGet && tarball) {
+      api.manageAttachment(req, resp, tarball);
+    } else if (isGet && module) {
+      api.manageModule(req, resp, module);
     } else {
       api.proxyToCentralRepository(req, resp);
     }
